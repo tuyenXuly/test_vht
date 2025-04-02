c32ef39118982d2dae5b029d98bcc033d4f1c00f
Pham Huy Tuyen <146762094+tuyenXuly@users.noreply.github.com>
2025-04-03 02:39:47 +0700

    Delete lcd_test directory

--- lcd_test/Debug/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.d
+++
@@ -1,6 +0,0 @@
-common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.o: \
- ../common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.c \
- ../common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- D:/testvht/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-../common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-D:/testvht/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
