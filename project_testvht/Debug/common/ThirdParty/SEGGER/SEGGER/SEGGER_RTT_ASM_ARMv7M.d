1c2f749f9fda660a0a942bc01873e6406f7a06f0
Pham Huy Tuyen <146762094+tuyenXuly@users.noreply.github.com>
2025-04-03 02:41:03 +0700

    Delete project_testvht directory

--- project_testvht/Debug/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.d
+++
@@ -1,6 +0,0 @@
-common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.o: \
- ../common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.S \
- ../common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- D:/testvht/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-../common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-D:/testvht/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
--- project_testvht/Debug/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.d
+++
@@ -1,6 +0,0 @@
-common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.o: \
- ../common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.c \
- ../common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- D:/testvht/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-../common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-D:/testvht/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
